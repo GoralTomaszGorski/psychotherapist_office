66c31bf7feca613078f96daa11d9b19affa35405
Tomasz GÓRSKI <98718498+GoralTomaszGorski@users.noreply.github.com>
2024-03-04 23:16:31 +0100

    email sender working
    tomorrow some methods I will split into another class to code clean

--- src/main/java/goral/psychotherapistoffice/domain/messeges/MessageService.java
+++ src/main/java/goral/psychotherapistoffice/domain/messeges/MessageService.java
@@ -1,6 +1,5 @@
 package goral.psychotherapistoffice.domain.messeges;
 
-import goral.psychotherapistoffice.domain.exception.MailSenderException;
 import goral.psychotherapistoffice.domain.messeges.dto.MessageDto;
 import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
@@ -13,19 +12,31 @@ import java.util.Properties;
 @Service
 public class MessageService{
 
-    public boolean sendMail(MessageDto messageDto) throws MessagingException {
+    public boolean sendMail(MessageDto messageDto) {
         boolean flag = false;
-        Properties properties = MailConfiguration.getConfiguration();
-//        MailAuthentication authentication = new MailAuthentication();
 
-        String username = "goralek_z_gor@o2.pl";
-        String password = "harnas";
+        //logic
+        //smtp properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+
+        final String username = "tomasz.gorski88@gmail.com";
+        final String password = "qruhprdwgxptvpmw";
+
+
+        //session
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(username, password);
             }
         });
+
+
+
         try {
             Message message = new MimeMessage(session);
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(messageDto.getRecipient()));
@@ -35,7 +46,7 @@ public class MessageService{
             Transport.send(message);
             flag = true;
         } catch (Exception e) {
-            throw new MailSenderException();
+            e.printStackTrace();
         }
         return flag;
     }
