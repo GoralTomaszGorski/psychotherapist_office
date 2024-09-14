e312f2aa2924191c025c57e460b77465007460bf
tomaszgorski <tomasz.gorski88@gmail.com>
2024-09-14 18:39:10 +0200

    Add assertJ and JUnit dependency

---
+++ src/test/java/goral/psychotherapistoffice/domain/calender/CalenderServiceTest.java
@@ -0,0 +1,12 @@
+package goral.psychotherapistoffice.domain.calender;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CalenderServiceTest {
+
+    @Test
+    void findAllFreeTherms() {
+
+    }
+}
