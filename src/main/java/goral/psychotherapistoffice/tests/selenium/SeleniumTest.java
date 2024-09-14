feb5aa511e6d10e8ed3cb5bd5f8d5fdac296e0da
tomaszgorski <tomasz.gorski88@gmail.com>
2024-09-14 14:19:00 +0200

    Add selenium dependency

---
+++ src/main/java/goral/psychotherapistoffice/tests/selenium/SeleniumTest.java
@@ -0,0 +1,8 @@
+package goral.psychotherapistoffice.tests.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    WebDriver driver = new ChromeDriver();
+}
