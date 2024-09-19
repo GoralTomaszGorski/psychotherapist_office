b2dd3f439b502749e8e5eceb1c3884dad72a80ec
tomaszgorski <tomasz.gorski88@gmail.com>
2024-09-19 21:13:29 +0200

    selenium test class props

--- src/test/java/selenium/SeleniumChromeTest.java
+++ src/test/java/selenium/SeleniumChromeTest.java
@@ -1,17 +1,23 @@
 package selenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
-
 
 public class SeleniumChromeTest {
     @Test
     public void openGooglePage() {
-
         String path = "/home/tomaszgorski/Dokumenty/chromedriver-linux64/chromedriver";
         System.setProperty("webdriver.chrome.driver", path);
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        String baseUrl = "https://www.psycholog-krasnik.pl";
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        WebDriver driver = new ChromeDriver(options);
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+        driver.close();
     }
 }
